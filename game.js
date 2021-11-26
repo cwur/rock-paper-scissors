d714b782cc9c3012ceb2f82b0a0ade578c81bd05
cwur <cwur@mailbox.org>
2021-11-26 20:16:49 +0100

    Refactor playRound

--- game.js
+++ game.js
@@ -6,32 +6,37 @@ function computerPlay() {
   return plays[index];
 }
 
+// playRound returns who has won the round.
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
 
-  const plays = ["rock", "paper", "scissors"];
-  const isInvalidByPlayer = !plays.includes(playerSelection)
-  if (isInvalidByPlayer) {
-    return `Invalid play by player: ${playerSelection}`
-  }
-  const isInvalidByComputer = !plays.includes(computerSelection)
-  if (isInvalidByComputer) {
-    return `Invalid play by computer: ${computerSelection}`
-  }
+  const isValidPlayByPlayer = ["rock", "paper", "scissors"].includes(playerSelection)
+  if (isValidPlayByPlayer === false) return `Invalid play by player: ${playerSelection}`;
+
+  const isValidPlayByComputer = ["rock", "paper", "scissors"].includes(computerSelection)
+  if (isValidPlayByComputer === false) return `Invalid play by computer: ${computerSelection}`;
 
   const isTie = playerSelection === computerSelection
-  if (isTie) {
-    return `Tie! Player and computer choose ${playerSelection}.`;
-  }
+  if (isTie) return `Tie! Player and computer choose ${playerSelection}.`;
 
-  switch (playerSelection) {
-    case "rock":
-      return computerSelection === "paper" ? `You loose!` : "You win!";
-    case "paper":
-      return computerSelection === "scissors" ? "You loose!" : "You win!";
-    case "scissors":
-      return computerSelection === "rock" ? "You loose!" : "You win!";
+  const rockBeatsScissors = playerSelection === "rock" & computerSelection === "scissors";
+  if (rockBeatsScissors) return getResultMessage(true, "rock", "scissors");
+
+  const scissorsBeatsPaper = playerSelection === "scissors" & computerSelection === "paper";
+  if (scissorsBeatsPaper) return getResultMessage(true, "scissors", "paper");
+
+  const paperBeatsRock = playerSelection === "paper" & computerSelection === "rock";
+  if (paperBeatsRock) return getResultMessage(true, "paper", "rock");
+
+  return getResultMessage(false, playerSelection, computerSelection);
+}
+
+function getResultMessage(doesPlayerWin, playerSelection, computerSelection) {
+  if (doesPlayerWin) {
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    return `You loose! ${playerSelection} is beaten by ${computerSelection}.`;
   }
 }
 
@@ -43,7 +48,8 @@ function game() {
 }
 
 console.log("tie", playRound("rock", "rock"));
-console.log("win", playRound("rock", "paper"));
+console.log("loose", playRound("rock", "paper"));
 console.log("invalid", playRound("rockz", "paper"));
 console.log("invalid", playRound("rock", "pap3r"));
 console.log("loose", playRound("scissors", "rock"))
+console.log("win", playRound("rock", "scissors"))
